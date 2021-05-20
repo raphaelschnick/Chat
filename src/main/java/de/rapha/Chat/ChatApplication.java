a3f4fdbded0aa2c0a2d6ebb40b0eb23b15649184
Schnick <rsc@sybit.de>
2021-05-20 11:25:10 +0200

    spring boot project

---
+++ src/main/java/de/rapha/Chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package de.rapha.Chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
